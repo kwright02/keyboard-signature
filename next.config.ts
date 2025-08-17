92401a262347495b243860881f7774ac31af4c61
Kwright02 <kyle.wright02@gmail.com>
2025-08-17 17:12:21 -0500

    Fixed config and chart label

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  devIndicators: false,
-  output: 'export'
+  devIndicators: false
 };
 
 export default nextConfig;
